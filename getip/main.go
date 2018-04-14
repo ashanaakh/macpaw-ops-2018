8360dce5ec9a33112af6d1cc3d2cd7cd9193925f
Ali Shanaakh <ashanaakh@gmail.com>
2018-04-14 19:09:15 +0300

    Fix getip

--- getip/main.go
+++ getip/main.go
@@ -1,35 +1,32 @@
 package main
 
 import (
-	// Standard library packages
-
 	"fmt"
 	"log"
+	"net"
 	"net/http"
-
-	"github.com/Tomasen/realip"
 )
 
-// func ipHandler(w http.ResponseWriter, req *http.Request) {
-// http
-// url := fmt.Sprintf(“%v %v %v”, r.Method, r.URL, r.Proto)
-// fmt.Fprint(req)
-// }
-
 func main() {
-	// r := mux.NewRouter()
-	// r.HandleFunc("/", ipHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		ip, port, err := net.SplitHostPort(req.RemoteAddr)
 
-	// http.Handle("/", r)
+		if err != nil {
+			fmt.Fprintf(w, "userip: %q is not IP:port", req.RemoteAddr)
+		}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		clientIP := realip.FromRequest(r)
-		// log.Println("GET / from", clientIP)
-		// fmt.Fprintf(w, "%v", r.Header.Get("X-Real-Ip"))
-		// fmt.Fprintf(w, "%v", r.Header.Get("X-Forwarded-For"))
-		fmt.Fprintf(w, "%v", clientIP)
-		fmt.Fprintf(w, "Hi")
+		userIP := net.ParseIP(ip)
+
+		if userIP == nil {
+			fmt.Fprintf(w, "userip: %q is not IP:port", req.RemoteAddr)
+			return
+		}
+
+		forward := req.Header.Get("X-Forwarded-For")
+
+		fmt.Fprintf(w, "<p>IP: %s</p>", ip)
+		fmt.Fprintf(w, "<p>Port: %s</p>", port)
+		fmt.Fprintf(w, "<p>Forwarded for: %s</p>", forward)
 	})
-
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
